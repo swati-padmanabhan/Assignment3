e26dcf57bb6a3f4a3a777e85dfec51a12f739d35
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-11 19:00:22 +0530

    initial commit of InOutFunction code

---
+++ InOutFunction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InOutParam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mainVariable = 10;
+            
+            InFunction(in mainVariable);
+            OutFunction(out mainVariable);
+            Console.WriteLine("The values after addition of IN Main is: " +mainVariable); //10
+            Console.WriteLine("The values after addition of OUT Main is: " + mainVariable); //20
+
+        }
+        static void OutFunction(out int outFunctionVariable)
+        {
+            outFunctionVariable = 0;
+            outFunctionVariable = outFunctionVariable + 20;
+            Console.WriteLine("The value after addition in Function is[out]: " +outFunctionVariable); //20
+        }
+        static void InFunction(in int inFunctionVariable)
+        {
+            //functionVariable = functionVariable + 20;
+            //The above line will show compile time error as we are trying to modify it.
+            Console.WriteLine("The value after adding in Function is[in]: " + inFunctionVariable); //20
+        }
+    }
+}
