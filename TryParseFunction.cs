381b4aafc003dd67b395f4709c23de034063bde4
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-11 19:00:43 +0530

    uploaded tryParse() function

---
+++ TryParseFunction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryParseFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber;
+            Console.WriteLine("Enter a number: ");
+
+            // TryParse() is used convert string to numeric types.
+
+            if (int.TryParse(Console.ReadLine(), out myNumber))
+                Console.WriteLine("Converted Successfully: " + myNumber);
+
+            else
+                Console.WriteLine("Could not convert, try again!");
+
+            // TryParse() is used to avoid exception handling if the conversion fails. 
+            Console.WriteLine("Thank you!");
+
+        }
+    }
+}
